1aef05f190f0a5d180a2325ebab6d5f72d28c6c1
KJH04_ <10wjdgp04@gmail.com>
2024-09-30 22:50:25 +0900

    배경 출력 등

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -5,4 +5,23 @@ open_canvas()
 tuk = load_image('TUK_GROUND.png')
 sheet = load_image('retro-character-sprite-sheet.png')
 
+def handle_events():
+    global run
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            run = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                run = False
+
+run = True;
+
+while run:
+    clear_canvas()
+    tuk.draw(400,300,800,600)
+    update_canvas()
+    handle_events()
+    delay(0.05)
+
 close_canvas()
