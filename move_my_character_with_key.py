c9110579d4f1d85e5fbc221c622fcbd8e4430e01
KJH04_ <10wjdgp04@gmail.com>
2024-09-30 23:26:34 +0900

    이동

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,7 +6,7 @@ tuk = load_image('TUK_GROUND.png')
 sheet = load_image('retro-character-sprite-sheet.png')
 
 def handle_events():
-    global run, dir, frame
+    global run, dir, frame_x
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -17,25 +17,45 @@ def handle_events():
             elif event.key == SDLK_DOWN:
                 dir = 3
             elif event.key == SDLK_UP:
-                dir = 0
+                dir = 4
             elif event.key == SDLK_LEFT:
                 dir = 1
             elif event.key == SDLK_RIGHT:
                 dir = 2
+        elif event.type ==SDL_KEYUP:
+            if dir == 1:
+                frame_x = 0
+            else:
+                frame_x = 1
+            dir = 0
 
 run = True;
-frame = 1
-dir = 3
+frame_x = 1
+frame_y = 3
+dir = 0
 pos_x = 800//2
 pos_y = 600//2
 
 while run:
     clear_canvas()
     tuk.draw(400,300,800,600)
-    sheet.clip_draw(frame*188//4,dir*245//4,188//4,245//4,pos_x,pos_y,70,80)
+    sheet.clip_draw(frame_x*188//4,frame_y*245//4,188//4,245//4,pos_x,pos_y,70,80)
     update_canvas()
     handle_events()
-    frame = (frame + 1) % 4
+    if dir != 0:
+        if dir == 3:
+            frame_y = 3
+            pos_y -= 10
+        elif dir == 4:
+            frame_y = 0
+            pos_y += 10
+        elif dir == 2:
+            frame_y = 2
+            pos_x += 10
+        elif dir == 1:
+            frame_y = 1
+            pos_x -= 10
+        frame_x = (frame_x + 1) % 4
     delay(0.05)
 
 close_canvas()
